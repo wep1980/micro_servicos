380f9a43326c5880f88052b0c339749105daae43
wep1980 <wepbike@gmail.com>
2021-10-18 21:28:40 -0300

    Projeto criado

---
+++ src/main/java/br/com/wepdev/recursoshumanostrabalhadores/RecursosHumanosTrabalhadoresApplication.java
@@ -0,0 +1,13 @@
+package br.com.wepdev.recursoshumanostrabalhadores;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecursosHumanosTrabalhadoresApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RecursosHumanosTrabalhadoresApplication.class, args);
+    }
+
+}
